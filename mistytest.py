24c46133c8f320bd311d205e104822bd1183f841
Tafarrel <kupuk23@gmail.com>
2024-11-14 14:56:15 +0100

    animate_CBF

---
+++ mistytest.py
@@ -0,0 +1,8 @@
+from mistyPy import Robot
+
+# Create a new mistyPy object
+mia = Robot("192.168.1.237")
+
+# mia.changeLED(red=255, green=255, blue=0)
+mia.moveHead(0, 0, 0, 100)
+# mia.changeImage("Convex-and-Non-Convex-sets.png")
